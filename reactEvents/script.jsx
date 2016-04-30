8393eee99b4223e89a1a740a2429ad200a03e551
Martin Ben <mebenav88@gmail.com>
2016-04-29 22:47:04 -0400

    event tutorial

--- reactEvents/script.jsx
+++ reactEvents/script.jsx
@@ -1,28 +1,39 @@
 // //example one
 // var Jumbotron = React.createClass({
+//   propType: {
+//     title: React.PropTypes.string.isRequired //this make the title property to accept string only and is required
+//   },
+//   getDefaultProps: function(){
+//     return {
+//       title: "hello world",
+//       text: "This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.",
+//     }
+//   },
 //   render: function(){
 //     return (
 //       <div className="jumbotron">
 //         <h1>{this.props.title}</h1>
 //         <p>{this.props.text}</p>
-//         <p><a className="btn btn-primary btn-lg" href={this.props.link} role="button">Learn more</a></p>
+//         <p><a onClick={this.onClick}className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+//
 //       </div>
 //     );
+//   },
+//   onClick: function(){
+//     alert('you clicked dude!');
 //   }
 // });
 // //in the render to get the properties, call this.props.propertyvariable
 //
 // //this react render takes two params the component, and the destination element
 //
-// ReactDOM.render(<Jumbotron title="This is the title"
-// text = "This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."
-// link="http://google.com"
-// />,
+// //title will overwrite the default
+// ReactDOM.render(<Jumbotron title="something else" />,
 // document.getElementById("jumbotron"));
 //
 // //end of example one
 
-//example two
+//example two with bind passing param
 var Jumbotron = React.createClass({
   propType: {
     title: React.PropTypes.string.isRequired //this make the title property to accept string only and is required
@@ -31,7 +42,6 @@ var Jumbotron = React.createClass({
     return {
       title: "hello world",
       text: "This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.",
-      link: "http://google.com"
     }
   },
   render: function(){
@@ -39,9 +49,13 @@ var Jumbotron = React.createClass({
       <div className="jumbotron">
         <h1>{this.props.title}</h1>
         <p>{this.props.text}</p>
-        <p><a className="btn btn-primary btn-lg" href={this.props.link} role="button">Learn more</a></p>
+        <p><a onClick={this.onClick.bind(this, "hello", "goodbye")}className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+
       </div>
     );
+  },
+  onClick: function(greeting, greeting2){
+    alert(greeting2);
   }
 });
 //in the render to get the properties, call this.props.propertyvariable
@@ -52,4 +66,4 @@ var Jumbotron = React.createClass({
 ReactDOM.render(<Jumbotron title="something else" />,
 document.getElementById("jumbotron"));
 
-//end of example two
+//end of example one
