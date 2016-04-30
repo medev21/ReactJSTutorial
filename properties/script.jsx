10afb21ff1ed3f0fe074d68f37598536bf0b5fce
Martin Ben <mebenav88@gmail.com>
2016-04-29 22:24:54 -0400

    react properties tutorail

--- properties/script.jsx
+++ properties/script.jsx
@@ -1,26 +1,55 @@
-//creating the Button component
-//react createClass takes multiple properties
+// //example one
+// var Jumbotron = React.createClass({
+//   render: function(){
+//     return (
+//       <div className="jumbotron">
+//         <h1>{this.props.title}</h1>
+//         <p>{this.props.text}</p>
+//         <p><a className="btn btn-primary btn-lg" href={this.props.link} role="button">Learn more</a></p>
+//       </div>
+//     );
+//   }
+// });
+// //in the render to get the properties, call this.props.propertyvariable
+//
+// //this react render takes two params the component, and the destination element
+//
+// ReactDOM.render(<Jumbotron title="This is the title"
+// text = "This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."
+// link="http://google.com"
+// />,
+// document.getElementById("jumbotron"));
+//
+// //end of example one
+
+//example two
 var Jumbotron = React.createClass({
-  getInitialState: function(){
-    return {counter: 0};
+  propType: {
+    title: React.PropTypes.string.isRequired //this make the title property to accept string only and is required
   },
-  handleClick: function(){
-    this.setState({ counter: this.state.counter + 1});
+  getDefaultProps: function(){
+    return {
+      title: "hello world",
+      text: "This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.",
+      link: "http://google.com"
+    }
   },
   render: function(){
     return (
-      // <button onClick={this.handleClick}>{this.state.counter}</button>
-      // in jsx instead of class, it needs to be className
       <div className="jumbotron">
-        <h1>Hello, world!</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
-        </p>
-        <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.text}</p>
+        <p><a className="btn btn-primary btn-lg" href={this.props.link} role="button">Learn more</a></p>
       </div>
     );
   }
 });
+//in the render to get the properties, call this.props.propertyvariable
 
 //this react render takes two params the component, and the destination element
-ReactDOM.render(<Jumbotron />, document.getElementById("jumbotron"));
+
+//title will overwrite the default
+ReactDOM.render(<Jumbotron title="something else" />,
+document.getElementById("jumbotron"));
+
+//end of example two
