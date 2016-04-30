6c356781d0a7d756087ca8388ec185d5b6c364bc
Martin Ben <mebenav88@gmail.com>
2016-04-30 15:29:16 -0400

    state and nesting tutorial

--- StateNesting/script.jsx
+++ StateNesting/script.jsx
@@ -1,69 +1,140 @@
-// //example one
-// var Jumbotron = React.createClass({
-//   propType: {
-//     title: React.PropTypes.string.isRequired //this make the title property to accept string only and is required
-//   },
-//   getDefaultProps: function(){
+// //example one w// getInitialState
+// var App = React.createClass({
+//   getInitialState: function(){
 //     return {
-//       title: "hello world",
-//       text: "This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.",
+//       text: 'hello world'
 //     }
 //   },
 //   render: function(){
 //     return (
-//       <div className="jumbotron">
-//         <h1>{this.props.title}</h1>
-//         <p>{this.props.text}</p>
-//         <p><a onClick={this.onClick}className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-//
+//       <div>
+//         <h1>{this.state.text}</h1>
+//         <button onClick={this.changeText} className="btn btn-primary">Click</button>
 //       </div>
-//     );
+//     )
 //   },
-//   onClick: function(){
-//     alert('you clicked dude!');
-//   }
+//   changeText: function(){
+//     this.setState({text: "welcome to my app"})
+//   }//this changes the from hello world when the button is clicked
 // });
 // //in the render to get the properties, call this.props.propertyvariable
 //
 // //this react render takes two params the component, and the destination element
 //
 // //title will overwrite the default
-// ReactDOM.render(<Jumbotron title="something else" />,
-// document.getElementById("jumbotron"));
+// ReactDOM.render(<App />, document.getElementById("app"));
 //
 // //end of example one
 
-//example two with bind passing param
-var Jumbotron = React.createClass({
-  propType: {
-    title: React.PropTypes.string.isRequired //this make the title property to accept string only and is required
-  },
-  getDefaultProps: function(){
+
+// //example two w// getInitialState and form
+// var App = React.createClass({
+//   getInitialState: function(){
+//     return {
+//       text: 'hello world'
+//     }
+//   },
+//   render: function(){
+//     return (
+//       <div>
+//         <h1>{this.state.text}</h1>
+//         <form>
+//           <input type="text" onChange={this.changeText} value={this.state.text} />
+//           <button className="btn btn-primary">Click</button>
+//         </form>
+//       </div>
+//     )
+//   },
+//   changeText: function(e){
+//     this.setState({text: e.target.value})
+//   }//this changes the text simutaneously while changing text in the input field
+// });
+// //in the render to get the properties, call this.props.propertyvariable
+//
+// //this react render takes two params the component, and the destination element
+//
+// //title will overwrite the default
+// ReactDOM.render(<App />, document.getElementById("app"));
+//
+// //end of example two
+
+// //example three w// getInitialState form and nesting
+// var App = React.createClass({
+//   getInitialState: function(){
+//     return {
+//       text: 'hello world'
+//     }
+//   },
+//   render: function(){
+//     return (
+//       <div>
+//         <h1>{this.state.text}</h1>
+//         <form>
+//           <input type="text" onChange={this.changeText} value={this.state.text} />
+//           <button className="btn btn-primary">Click</button>
+//         </form>
+//         <br />
+//         <ComponentTwo />
+//       </div>
+//     )
+//   }, //you can nest component inside component <ComponentTwo /> is calling ComponentTwo variable
+//
+//   changeText: function(e){
+//     this.setState({text: e.target.value})
+//   }//this changes the text simutaneously while changing text in the input field
+// });
+//
+// var ComponentTwo = React.createClass({
+//   render: function(){
+//     return (
+//       <div>
+//         Component two
+//       </div>
+//     )
+//   }
+// });
+//
+// ReactDOM.render(<App />, document.getElementById("app"));
+//
+// //end of example three
+
+
+//example four w// getInitialState passing properties in nesting
+var App = React.createClass({
+  getInitialState: function(){
     return {
-      title: "hello world",
-      text: "This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.",
+      text: 'hello world'
     }
   },
   render: function(){
     return (
-      <div className="jumbotron">
-        <h1>{this.props.title}</h1>
-        <p>{this.props.text}</p>
-        <p><a onClick={this.onClick.bind(this, "hello", "goodbye")}className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-
+      <div>
+        <h1>{this.state.text}</h1>
+        <form>
+          <input type="text" onChange={this.changeText} value={this.state.text} />
+          <button className="btn btn-primary">Click</button>
+        </form>
+        <br />
+        <ComponentTwo text={this.state.text}/>
       </div>
-    );
-  },
-  onClick: function(greeting, greeting2){
-    alert(greeting2);
+    )//this text is a property that gets that state's text
+  }, //you can nest component inside component <ComponentTwo /> is calling ComponentTwo variable
+
+  changeText: function(e){
+    this.setState({text: e.target.value})
+  }//this changes the text simutaneously while changing text in the input field
+});
+
+var ComponentTwo = React.createClass({
+  render: function(){
+    return (
+      <div>
+        {this.props.text}
+      </div>
+    )//this props text calls the ComponentTwo xml
   }
 });
-//in the render to get the properties, call this.props.propertyvariable
 
-//this react render takes two params the component, and the destination element
+ReactDOM.render(<App />, document.getElementById("app"));
 
-//title will overwrite the default
-ReactDOM.render(<Jumbotron title="something else" />,
-document.getElementById("jumbotron"));
-
-//end of example one
+//end of example three
